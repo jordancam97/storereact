3f477fa177e48c02fdc16bbd399fb99a1e139ed2
Xolarix <vortexlabcol@gmail.com>
2022-10-26 01:48:20 -0500

    header draft

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="bg-red boxHeader d-flex row justify-content-between align-items-center w-100">
+      <div className="h-100"> 
+        <img
+          className="objectFit h-100"
+          src={require("../assets/img/logo512.png")}
+          alt=""
+        />
+      </div>
+      <div className="colorPru">
+        hola
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
