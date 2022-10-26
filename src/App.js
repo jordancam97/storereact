c834e73948740d9f6cd6aaa9829c2e3bb862c74f
Xolarix <vortexlabcol@gmail.com>
2022-10-26 00:33:39 -0500

    add css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import "./css/styles/styles.css";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='colorPru'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
